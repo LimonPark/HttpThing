fbaf0e9071db3f084abcca1bbfa9c93a4680566e
nikitakafes <kafes-nik@mail.ru>
2024-02-09 16:41:00 +0300

    Initial Commit

---
+++ HttpThing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HttpThing
+//
+//  Created by Nikita Hasan Kafes on 09.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
